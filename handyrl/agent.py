090957d079f9fe33dffeaa6829f68a5e1e7d28db
akane-minami <akane03730913@gmail.com>
2024-02-13 14:36:14 +0900

    :sparkles: Add R4DAgent

--- handyrl/agent.py
+++ handyrl/agent.py
@@ -18,6 +18,8 @@ def agent_class(args):
         return QAgent
     elif args['type'] == 'RSRS' or args['type'] == 'RSRS-RND':
         return RSRSAgent
+    elif args['type'] == 'R4D-RSRS' or args['type'] == 'R4D-RSRS-RND':
+        return R4DRSRSAgent
     # elif args['type'] == 'SRS' or args['type'] == 'SRS-RND':
     #     return SRSAgent
     else:
@@ -293,7 +295,113 @@ class RSRSAgent(Agent):
 
         return action
 
+class R4DRSRSAgent(Agent):
+    def __init__(self, model, metadataset, role='e', temperature=None, observation=True, args=None):
+        super().__init__(model, metadataset, role, temperature, observation, args)
+        self.metadata_keys = ['latent', 'action']
+        self.global_aleph = metadataset.get('global_aleph', 1.0)
+        self.rw = metadataset.get('regional_weight', 0.0)
 
+    def reset(self, env, show=False):
+        self.hidden = self.model.init_hidden()
+        return self.metadata_keys
+
+    def action(self, env, player, show=False, action_log=None):
+        # learning = (action_log is not None)
+        global_aleph = self.global_aleph
+        if action_log is not None:
+            global_v = action_log['global_v'][player]
+            global_delta = np.amax([global_aleph - global_v, 0.0])
+        else:
+            global_v = None
+            global_delta = 0.0
+        obs = env.observation(player)
+        outputs = self.plan(obs) # reccurent model 対応
+        actions = env.legal_actions(player)
+        v = outputs.get('value', None)
+        p_nn = outputs['policy']
+        q = outputs.get('qvalue', None)
+        aleph = global_delta + np.amax(q) if q is not None else 0.0
+        c_predict = outputs.get('confidence_57', None)
+        c_target = outputs.get('confidence_57_fix', None)
+
+        # 分割
+        c_predict = c_predict.reshape(-1, actions.size)
+        c_target = c_target.reshape(-1, actions.size)
+        # 計算
+        rnd = np.mean((c_target - c_predict)**2,axis=0)
+        rnd = 1e-6/(rnd+1e-6)
+        #正規化
+        c_nn = rnd/np.sum(rnd)
+
+        latent = outputs['latent']
+        if 'knn' in self.metadataset:
+            c_reg = self.metadataset['knn'].regional_nn(latent)
+            if c_reg is not None:
+                c = self.rw * c_reg.squeeze() + (1.0 - self.rw) * c_nn
+            else:
+                p_reg = 1.0/actions.size
+                c_reg = np.full(actions.size, p_reg)
+                c = c_nn
+        else:
+            p_reg = 1.0/actions.size
+            c_reg = np.full(actions.size, p_reg)
+            c = c_nn
+
+        # rs = c * (q - aleph)
+        if np.amax(q) >= aleph:
+            #print("満足")
+            fix_aleph = np.amax(q) + sys.float_info.epsilon
+            delta = fix_aleph - q
+            if np.amin(delta) < 0:
+                delta -= np.amin(delta)  # 丸め誤差が発生した場合の処理
+            if np.any(delta == 0.0):
+                delta += sys.float_info.epsilon
+        else:
+            #print("非満足")
+            delta = aleph - q
+        z = 1.0 / np.sum(1.0 / delta)
+        rho = z / delta #17
+        rsrs = (np.max(c / rho) + sys.float_info.epsilon) * rho - c
+        if np.min(rsrs) < 0:
+            rsrs -= np.min(rsrs)
+        if np.any(rsrs == 0.0):
+            rsrs += sys.float_info.epsilon
+        p = np.log(rsrs)
+        action_mask = np.ones_like(p) * 1e32
+        action_mask[actions] = 0
+        p = p - action_mask
+        p_srs = softmax(p)
+        entropy_srs = -(p_srs * np.ma.log(p_srs)/np.ma.log(len(p))).sum()
+
+        if self.generating or self.temperature != 0.0:
+            policy = softmax(p) if self.temperature is None else softmax(p / self.temperature)
+            action = random.choices(np.arange(len(p)), weights=policy)[0]
+            selected_prob = policy[action]
+            if show:
+                print_outputs(env, policy, v)
+        else:
+            ap_list = sorted([(a, p[a]) for a in actions], key=lambda x: -x[1])
+            action = ap_list[0][0]
+            selected_prob = 1.0
+            if show:
+                print_outputs(env, softmax(p), v)
+        one_hot_action = np.identity(len(p))[action]
+
+        # action log は action 決定過程の情報
+        if self.generating:
+            action_log['moment']['observation'][player] = obs
+            action_log['moment']['value'][player] = v
+            # TODO 満足していると 0.0 が入っちゃう問題
+            action_log['moment']['selected_prob'][player] = selected_prob
+            action_log['moment']['action_mask'][player] = action_mask
+            action_log['metadata']['latent'][player] = latent
+            action_log['metadata']['action'][player] = one_hot_action
+            action_log['moment']['c'][player] = c
+            action_log['moment']['c_reg'][player] = c_reg
+            # action_log['metadata']['entropy_srs'][player] = entropy_srs
+
+        return action
 
 class EnsembleAgent(Agent):
     def reset(self, env, show=False):
